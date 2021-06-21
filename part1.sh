d1e9af7d0dd42c5f8b06ca3b577c0442c8049645
davidenkovic <davidenkovic100@gmail.com>
2021-06-21 23:01:29 +0200

    wip

--- part1.sh
+++ part1.sh
@@ -8,6 +8,6 @@ mkdir -p docs/images;
 cp -r -p -v asciidocs/docinfo.html $BUILD_DIR;
 
 cp -r -p -v asciidocs/*.adoc $BUILD_DIR;
-cp -r -p -v asciidocs/jpa/*.adoc $BUILD_DIR;
+cp -r -p -v asciidocs/*.adoc $BUILD_DIR;
 
 cp -r -p -v asciidocs/images/* $BUILD_DIR/images;
